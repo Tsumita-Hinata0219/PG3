58ad6b83ae895385e857a0db3f6306fbd59032c2
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-11-13 12:11:36 +0900

    naosita

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main() {
 
 	Calc<int, int> val1(39, 42);
 	Calc<int, float> val2(16, 34.0f);
-	Calc<int, double> val3(12, 89.0f);
+	Calc<int, double> val3(12, 89.0);
 	Calc<float, float> val4(129.0f, 34.0f);
-	Calc<float, double> val5(23.0f, 456.0f);
-	Calc<double, double> val6(39.0f, 29.0f);
+	Calc<float, double> val5(23.0f, 456.0);
+	Calc<double, double> val6(39.0, 29.0);
 
 	
 	std::cout << "val1 <int, int>       : " << val1.Min() << std::endl;
