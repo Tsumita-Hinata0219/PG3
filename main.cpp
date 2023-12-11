8db6a52e3788826e1e6fb554c6c8b0e63732752f
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-12-11 11:46:32 +0900

    できたけど、見づらいからあとで書き直す

--- main.cpp
+++ main.cpp
@@ -1,6 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <list>
+#include <iostream>
+
 
 int main() {
 
+	// 初期リスト
+	std::list<const char*> lst = {
+		"Tokyo","Kanda","Akihabara","Okachimachi","Ueno","Uguisudani","Nippori", "Tabata","Komagome",
+		"Sugamo","Otsuka","Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
+		"Ebisu","Meguro","Gotanda","Osaki","Shinagawa", "Tamachi","Hamamatsucho","Shimbashi","Yurakucyo",
+	};
+
+
+	// 初期リストの出力
+	std::cout << "1970" << std::endl;
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+
+		std::cout << *itr << std::endl;
+	}
+
+
+
+	// リストに新たに追加
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+
+		if (*itr == "Tabata") {
+
+			itr = lst.insert(itr, "Nishi-Nipporo");
+
+			++itr;
+		}
+	}
+
+	// 追加後のリストの描画
+	std::cout << "\n2019" << std::endl;
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+
+		std::cout << *itr << std::endl;
+	}
+
+
+
+	// リストに新たに追加
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+
+		if (*itr == "Tamachi") {
+
+			itr = lst.insert(itr, "Takanawa GateWay");
+
+			++itr;
+		}
+	}
+
+	// 追加後のリストの描画
+	std::cout << "\n2022" << std::endl;
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+
+		std::cout << *itr << std::endl;
+	}
 
 
 	return 0;
