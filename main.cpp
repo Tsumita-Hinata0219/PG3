0b699b7850ab719582481327136e3a9a36595e29
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-01-15 11:12:22 +0900

    できた

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+#include <functional>
+#include <thread>
+
+
+std::function<void(std::string)> TextPrint = [=](std::string text) {
+	std::cout << text << std::endl;
+};
+
 
 int main() {
 
+	std::thread th1(TextPrint, "thread 1");
+	th1.join();
 
+	std::thread th2(TextPrint, "thread 2");
+	th2.join();
+
+	std::thread th3(TextPrint, "thread 3");
+	th3.join();
 
 	return 0;
 }
