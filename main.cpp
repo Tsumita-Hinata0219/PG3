06c01b6ac9dd32151ded54da66ecd86aa3d21d41
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-01-29 11:14:43 +0900

    std::coutの使い方忘れちゃった

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <chrono>
+
 
 int main() {
 
+	std::chrono::system_clock::time_point  start, end;
 
+	std::string a(1000000, 'a');
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	// 処理
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	std::cout << "%d" << end << std::end;
 
 	return 0;
 }
