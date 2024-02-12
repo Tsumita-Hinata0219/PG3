b41ddbb0649c5750498cf978ba99662a96ed0091
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-02-12 19:22:44 +0900

    ok

--- main.cpp
+++ main.cpp
@@ -1,35 +1,33 @@
-#include "CalcSalary.h"
+#include <stdio.h>
+#include <iostream>
+
+
+// 再帰的な賃金体系の時給計算
+int CalculateRecursionSalary(int salary, int workTime) {
+
+    //std::cout << "再帰的な賃金体系 : " << salary << std::endl;
+
+    if (workTime <= 1) {
+        return salary;
+    }
+    return salary + CalculateRecursionSalary(salary * 2 - 50, workTime - 1);
+}
 
 int main() {
 
-	CalcSalary* calc = new CalcSalary();
+	// 働く時間
+	const int workTime = 8;
 
-	int workTime = 1;
-	calc->CalculateAndPrintSalary(workTime);
+	// 一般的賃金体系の時給
+	const int hourlySalary = 1072;
 
-	return 0;
+	// 各体系の時給計算
+	int normalSalary_ = hourlySalary * workTime;
+	int recursionSalary_ = CalculateRecursionSalary(100, workTime);
+
+	std::cout << "働いた時間 : " << workTime << "時間\n";
+	std::cout << "一般的な賃金体系 : " << normalSalary_ << std::endl;
+	std::cout << "再帰的な賃金体系 : " << recursionSalary_ << std::endl;
+
+	return  0;
 }
-
-//
-//int  RecursiveSalary(int wage, int workingTime)
-//{
-//	if (workingTime <= 1) {
-//		return wage;
-//	}
-//	return wage + RecursiveSalary(wage * 2 - 50, workingTime - 1);
-//}
-//
-//int main()
-//{
-//	const int workingTime = 8;
-//	const int hourlyWage = 1072;
-//
-//	int NormalTotalWage = hourlyWage * workingTime;
-//	int RecursiveTotalWage = RecursiveSalary(100, workingTime);
-//
-//	printf("%d時間働いたときの給料\n", workingTime);
-//	printf("一般的賃金体系の給料 : %d\n", NormalTotalWage);
-//	printf("再起的賃金体系の給料 : %d\n", RecursiveTotalWage);
-//
-//	return  0;
-//}
